03680d1621cc4924e3aed5d99e620f56a7d83be0
huangjian <huangjian@bytedance.com>
2018-07-08 13:32:48 +0800

    first commit
    Change-Id: I2a0a7541b3041f4c0dca0e25d43be0a730556b9f

---
+++ src/main/kotlin/com/easy/HelloWorld.kt
@@ -0,0 +1,12 @@
+package com.easy
+
+import java.util.Date
+import java.text.SimpleDateFormat
+
+fun main(args: Array<String>) {
+    val a: Int = 1
+    val b = 2
+    val c: Int
+    c = 3
+    println("a=$a,")
+}
